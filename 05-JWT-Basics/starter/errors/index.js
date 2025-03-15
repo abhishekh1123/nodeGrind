a40714ccb804afd4a173d197c1d5a11372bd18b3
Abhishekh <abhishekh23kumar@gmail.com>
2025-03-15 21:21:42 +0530

    JWT Basics

---
+++ 05-JWT-Basics/starter/errors/index.js
@@ -0,0 +1,10 @@
+const CustomAPIError = require('./custom-error')
+const BadrequestError = require('./bad-request')
+const UnaunthenticatedError = require('./unaunthenticated')
+
+
+module.exports = {
+  CustomAPIError,
+  BadrequestError,
+  UnaunthenticatedError
+}
